a951fe90edd18ffff9ca8e5741b64ce4ad7bbc7c
SusKeyyy <166961563+SysKeyyyJS@users.noreply.github.com>
2024-11-07 09:19:31 -0500

    Buncha Stuff Added or smth idk like radio website

---
+++ fusionfm/assets/script.js
@@ -0,0 +1,4 @@
+// Toggle sidebar visibility
+document.getElementById("sidebarToggle").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.toggle("active");
+});
